fb07cd4fbcfad629d863fb59eabe4817467cb731
Ellen Zhang <Ellenzhang0827@gmail.com>
2023-11-20 21:57:07 -0800

    added front end base code, fixed an error in REST endpoint

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,21 +1,25 @@
+import React,{useState,useEffect} from 'react'
+
+
 let httpRequest;
 
-document.getElementById("user1_button").addEventListener("Explore a professor's courses", handleUser1Button);
-document.getElementById("user2_button").addEventListener("Find out the best professor to take a course with", handleUser2Button);
+document.getElementById("user1_button").addEventListener("click", handleUser1Button);
+document.getElementById("user2_button").addEventListener("click", handleUser2Button);
 
 let inputbtn = document.getElementById("input")
 function handleUser1Button() {
 	let name  = inputbtn.innerText;
-	let baseURI = "http://dfghj/courses/profs/alex"
+	//let baseURI = "http://dfghj/courses/profs/alex"
 	// httpRequest = new XMLHttpRequest();
 	// httpRequest.send( {
 	//
 	//
 	// 	`${baseURI}/${name}`
 	// })
-	// httpRequest.open("GET", "/style.css");
-	//httpRequest.send();
+
 }
+
+
 
 function handleUser2Button() {
 	//alert("");
